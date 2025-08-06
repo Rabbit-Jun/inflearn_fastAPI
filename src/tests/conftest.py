69cab574020e8eed79a3ef99f0610125d04288ca
JunEon <junun24@gmail.com>
2025-08-06 17:25:39 +0900

    test

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -4,6 +4,6 @@ from fastapi.testclient import TestClient
 from main import app
 
 
-@pytest.fixture
+@pytest.fixture # 여러곳에서 사용 가능하게 만듬
 def client():
     return TestClient(app=app)
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -31,4 +31,16 @@ def test_get_todos(client, mocker ):
     {'id': 2,'contents': 'FastAPI Section 1', 'is_done': False},
     {'id': 1,'contents': 'FastAPI Section 0',  'is_done': True},
         ]
-        } # 위쪽에 문제가 생기면 여기까지 테스트 안됨 
+        } # 위쪽에 문제가 생기면 여기까지 테스트 안됨
+
+def test_get_todo(client, mocker):
+    mocker.patch("main.get_todo_by_todo_id", return_value=ToDo(id=1, contents="todo", is_done=True))
+    response = client.get("/todos/1")
+    assert response.status_code == 200
+    assert response.json() == {'id': 1, 'contents': 'todo', 'is_done': True}
+
+    #404
+    mocker.patch("main.get_todo_by_todo_id", return_value=None)
+    response = client.get("/todos/1")
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Todo not found'}
