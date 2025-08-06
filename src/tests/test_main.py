388c8adcce82e0646c6300d47edde9ae51aa7241
JunEon <junun24@gmail.com>
2025-08-06 17:58:30 +0900

    test_delete

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -46,7 +46,7 @@ def test_get_todo(client, mocker):
     assert response.json() == {'detail': 'Todo not found'}
 
 def test_create_todo(client, mocker):
-    create_spy = mocker.spy(ToDo, "create") # main.py에서 create_todo_handler 함수가 호출될 때 ToDo.create 메서드를 트래킹
+    create_spy = mocker.spy(ToDo, "create") # main.py에서 create_todo_handler 함수를 실제로 실행하면서 감시
     mocker.patch("main.create_todo", return_value=ToDo(id=1, contents="todo", is_done=False))
     body ={
         "contents": "test",
@@ -60,3 +60,56 @@ def test_create_todo(client, mocker):
 
     assert response.status_code == 201
     assert response.json() == {'id': 1, 'contents': 'todo', 'is_done': False}
+
+def test_update_todo(client, mocker):
+    mocker.patch("main.get_todos",return_value=[
+        ToDo(id=1, contents="FastAPI Section 0", is_done=True),
+        ToDo(id=2, contents="FastAPI Section 1", is_done=False),
+    ]) #실제로 동작하지는 않지만 동작하는 것처럼
+    response = client.get("/todos")
+    assert response.status_code == 200
+    assert response.json() == {'todos': [
+    {'id': 1,'contents': 'FastAPI Section 0',  'is_done': True},
+    {'id': 2,'contents': 'FastAPI Section 1', 'is_done': False},
+
+]} # 실제 데이터의 모든 값들을 다 넣어줘야 함 vscod에서 실험관 모형에서 test 해볼 수 있음(테스트를 위한 db또는 모킹을 만들어야 함)
+    # order=DESC로 요청하면 역순으로 정렬된 결과를 반환하는지 확인
+    response = client.get("/todos?order=desc") # main.py에서 get_todos_handler 함수가 호출됨
+
+    assert response.status_code == 200
+    assert response.json() == {'todos': [
+    {'id': 2,'contents': 'FastAPI Section 1', 'is_done': False},
+    {'id': 1,'contents': 'FastAPI Section 0',  'is_done': True},
+        ]
+        } # 위쪽에 문제가 생기면 여기까지 테스트 안됨
+
+def test_update_todo(client, mocker):
+    mocker.patch("main.get_todo_by_todo_id", return_value=ToDo(id=1, contents="todo", is_done=True))
+    undone = mocker.patch.object(ToDo, "undone")
+    mocker.patch("main.update_todo", return_value=ToDo(id=1, contents="todo", is_done=False))
+    response = client.patch("/todos/1", json={"is_done": False})
+
+    undone.assert_called_once_with()  # undone 메서드가 호출되었는지 확인
+
+    assert response.status_code == 200
+    assert response.json() == {'id': 1, 'contents': 'todo', 'is_done': False}
+
+    #404
+    mocker.patch("main.get_todo_by_todo_id", return_value=None)
+    response = client.patch("/todos/1", json={"is_done": True})
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Todo not found'}
+
+def test_delete_todo(client, mocker):
+    #204
+    mocker.patch("main.get_todo_by_todo_id", return_value=ToDo(id=1, contents="todo", is_done=True))
+    mocker.patch("main.delete_todo", return_value=None)  # delete_todo 함수가 실제로 동작하지 않도록 Mock
+    response = client.delete("/todos/1")
+    assert response.status_code == 204
+    
+
+    #404
+    mocker.patch("main.get_todo_by_todo_id", return_value=None)
+    response = client.delete("/todos/1")
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Todo not found'}
