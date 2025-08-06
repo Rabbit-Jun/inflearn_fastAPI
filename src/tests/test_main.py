67105a6e6022325c86aa3ebc4470ba686da3dde2
JunEon <junun24@gmail.com>
2025-08-06 17:05:34 +0900

    mocking

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
-
 from  main import app
+from database.orm import ToDo
 
 
 client = TestClient(app=app) # main.py에서 FastAPI 인스턴스를 가져와서 테스트 클라이언트 생성
@@ -11,24 +11,25 @@ def test_health_check():
     assert response.status_code == 200
     assert response.json() == {"ping": "pong"}  
 
-def test_get_todos():
+def test_get_todos(mocker):
+
+    mocker.patch("main.get_todos",return_value=[
+        ToDo(id=1, contents="FastAPI Section 0", is_done=True),
+        ToDo(id=2, contents="FastAPI Section 1", is_done=False),
+    ]) #실제로 동작하지는 않지만 동작하는 것처럼
     response = client.get("/todos")
     assert response.status_code == 200
     assert response.json() == {'todos': [
-    {'contents': 'FastAPI Section 0', 'id': 1, 'is_done': True},
-    {'contents': 'FastAPI Section 1', 'id': 2, 'is_done': True},
-    {'contents': 'FastAPI Section 2', 'id': 3, 'is_done': False},
-    {'contents': 'test', 'id': 5, 'is_done': True},
-    {'contents': 'testg', 'id': 6, 'is_done': True}
-]} # 실제 데이터의 모든 값들을 다 넣어줘야 함 vscod에서 실험관 모형에서 test 해볼 수 있음
+    {'id': 1,'contents': 'FastAPI Section 0',  'is_done': True},
+    {'id': 2,'contents': 'FastAPI Section 1', 'is_done': False},
+
+]} # 실제 데이터의 모든 값들을 다 넣어줘야 함 vscod에서 실험관 모형에서 test 해볼 수 있음(테스트를 위한 db또는 모킹을 만들어야 함)
     # order=DESC로 요청하면 역순으로 정렬된 결과를 반환하는지 확인
     response = client.get("/todos?order=desc") # main.py에서 get_todos_handler 함수가 호출됨
+
     assert response.status_code == 200
     assert response.json() == {'todos': [
-        {'contents': 'testg', 'id': 6, 'is_done': True},
-        {'contents': 'test', 'id': 5, 'is_done': True},
-        {'contents': 'FastAPI Section 2', 'id': 3, 'is_done': False},
-        {'contents': 'FastAPI Section 1', 'id': 2, 'is_done': True},
-        {'contents': 'FastAPI Section 0', 'id': 1, 'is_done': True},
+    {'id': 2,'contents': 'FastAPI Section 1', 'is_done': False},
+    {'id': 1,'contents': 'FastAPI Section 0',  'is_done': True},
         ]
         } # 위쪽에 문제가 생기면 여기까지 테스트 안됨 
